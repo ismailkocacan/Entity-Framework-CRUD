d7f385e873ba8dbc99694b861da8a6d93c328a12
İsmail Kocacan <ismailkocacan@gmail.com>
2013-09-08 00:52:42 +0300

    - data katmanı eklendi. MyDbContext

---
+++ IsmailKocacan.EfOrnek.Data/DatabaseContext/MyDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace IsmailKocacan.EfOrnek.Data.DatabaseContext
+{
+    public class MyDbContext : DbContext
+    {
+
+        public MyDbContext()
+        {
+            this.Database.CreateIfNotExists();
+            this.Configuration.LazyLoadingEnabled = true;
+        }
+
+
+        // factory method
+        public static MyDbContext create()
+        {
+            var context = new MyDbContext();
+            return context;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+Entity Framework CRUD 
+===========================
+
+Entity Framework kullanarak bir Kullanici sınıfı üzerinde CRUD işlemlerini gösteren örnektir.
